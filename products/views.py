33361f546e172bc709285c65e6936bafce24caee
Jacob Yu <jacob.yu@DEV122.texturallc.net>
2019-04-15 11:21:53 -0500

    product hunt clone for practice

--- products/views.py
+++ products/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products': products})
+
+
+@login_required(login_url="/accounts/signup")
+def create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        url = request.POST['url']
+        icon = request.FILES['icon']
+        image = request.FILES['image']
+        if title and body and url and icon and image:
+            product = Product()
+            product.title = title
+            product.body = body
+            product.icon = icon
+            product.image = image
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/' + str(product.id))
+        else:
+            return render(request, 'products/create.html', {'error': 'Please fill out all fields'})
+    else:
+        return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product': product})
+
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == "POST":
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
